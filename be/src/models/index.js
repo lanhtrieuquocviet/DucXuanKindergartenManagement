88eefb75dad28a64184778c76d3cbda42c5a6641
lanhtrieuquocviet <vietltqhe173238@fpt.edu.vn>
2026-02-10 17:12:44 +0700

    lam form attendance

--- be/src/models/index.js
+++ be/src/models/index.js
@@ -5,6 +5,7 @@ const Student = require('./Student');
 const Grade = require('./Grade');
 const Classes = require('./Classes');
 const AcademicYear = require('./AcademicYear');
+const Attendances = require('./Attendances');
 
 module.exports = {
   User,
@@ -13,5 +14,6 @@ module.exports = {
   Student,
   Grade,
   Classes,
-  AcademicYear
+  AcademicYear,
+  Attendances
 };
