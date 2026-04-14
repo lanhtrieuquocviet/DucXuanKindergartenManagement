eb245445c3982fa02f4af70cad5e00e0994a6683
Tienthinh2608 <tienthinh66899@gmail.com>
2026-04-14 23:34:39 +0700

    Fix be

--- be/src/controller/contactController.js
+++ be/src/controller/contactController.js
@@ -1,9 +1,9 @@
 const service = require('../services/contactService.js');
 
-const validateSubmitContact = async (req, res, next) => service.validateSubmitContact(req, res, next);
+const validateSubmitContact = service.validateSubmitContact;
 const submitContact = async (req, res, next) => service.submitContact(req, res, next);
 const listContacts = async (req, res, next) => service.listContacts(req, res, next);
-const validateReplyContact = async (req, res, next) => service.validateReplyContact(req, res, next);
+const validateReplyContact = service.validateReplyContact;
 const replyContact = async (req, res, next) => service.replyContact(req, res, next);
 const clearReplyContact = async (req, res, next) => service.clearReplyContact(req, res, next);
 const resendReplyEmail = async (req, res, next) => service.resendReplyEmail(req, res, next);
